1d809760e0d71e9fde5e9b97d37164f2ca4f20a5
Luiz Ladeira <luiz.ladeira@fonelight.com.br>
2015-10-20 15:30:33 -0200

    Primeiro commit. Teste de Fibonacci de 0 e sua implementacao

---
+++ src/devcast/palestra/git/Fibonacci.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package devcast.palestra.git;
+
+/**
+ *
+ * @author LuizLadeira
+ */
+public class Fibonacci {
+    
+    
+    public int calcularFibonacci(int numero){
+        return 0;
+    }
+}
